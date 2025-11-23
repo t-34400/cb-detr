654489c1afa6f702068b95cdd1cdf9d03766a7e7
t-34400 <34400mt@gmail.com>
2025-11-24 04:11:47 +0900

    Add __init__.py to expose model components

---
+++ cbdetr/__init__.py
@@ -0,0 +1,9 @@
+from cbdetr.models.build_model import build_model, ModelArgs, TransformerArgs
+from cbdetr.models.cbdetr import CuboidDetr
+
+__all__ = [
+    "build_model",
+    "ModelArgs",
+    "TransformerArgs",
+    "CuboidDetr",
+]
